91d60d0d05e1a866b4c046eec865f947fc94667e
Sadraw <sadrathescientist@gmail.com>
2023-03-27 13:27:54 +0200

    final touch

--- js/script.js
+++ js/script.js
@@ -15,7 +15,43 @@ dragElement(document.getElementById('plant14'));
 
 
 const myButton = document.getElementByID('myButton');
-myButton.addEventlistner('Click', myButton);
+myButton.addEventlistener('Click', myButton);
 function myFunction() {
     console.log('Hello');
+}
+
+function dragElement(terrariumElement){
+    //set 4 positions for positioning on the screen 
+    let pos1 = 0;
+    let pos2 = 0;
+    let pos3 = 0;
+    let pos4 = 0;
+    terrariumElement.onpointerdown = pointerDrag;
+
+    function pointerDrag(e){
+        e.preventDefault();
+        console.log(e);
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onpointermove = elementDrag;
+        document.onpointerup   = stopElementDrag;
+
+    }
+
+    function elementDrag(e) {
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        console.log(pos1, pos2, pos3, pos4);
+        terrariumElement.style.top  = terrariumElement.offsetTop - pos2 + 'px';
+        terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
+
+    }
+
+    function stopElementDrag() {
+        document.onpointerup = null;
+        document.onpointermove = null;
+    }
+
 }
